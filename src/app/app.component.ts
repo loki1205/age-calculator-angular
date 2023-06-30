c74d4747061044efea442c4753723815ec394518
Loknath Mishra <loknath.mishra@framsikt.no>
2023-06-30 15:15:43 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ButtonThemeColor } from '@progress/kendo-angular-buttons';
+import { FormatSettings } from '@progress/kendo-angular-dateinputs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-age-calculator';
+  value!:Date;
+  public format: FormatSettings = {
+    displayFormat: "dd/MM/yyyy",
+    inputFormat: "dd/MM/yyyy",
+  };
+  age!: number;
+  color:ButtonThemeColor="primary"
+  handleInput(){
+    const dob = new Date(this.value);
+    const timeDiff = Math.abs(Date.now() - dob.getTime());
+    this.age = Math.floor((timeDiff / (1000 * 3600 * 24))/365);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DateInputsModule } from "@progress/kendo-angular-dateinputs";
+import '@angular/localize/init'
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputsModule,
+    BrowserAnimationsModule,
+    DateInputsModule,
+    ButtonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
